d53faafa13379e7c6583ae8c7cd3318419219d12
Sarah Dulat <sarah.dulat@gmail.com>
2016-12-02 17:33:24 -0500

    tried to install compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,40 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee'),
-	browserify = require('gulp-browserify'),
-	concat = require('gulp-concat');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    concat = require('gulp-concat');
 
-var coffeeSources = ['componants/coffee/tagline.coffee'];
+var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
-	'componants/scripts/rclick.js',
-	'componants/scripts/pixgrid.js',
-	'componants/scripts/tagline.js',
-	'componants/scripts/tempalte.js'
-	];
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
-	gulp.src(coffeeSources)
-		.pipe(coffee({bare: true})
-			.on('error', gutil.log))
-		.pipe(gulp.dest('componants/scripts'))
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
-	gulp.src(jsSources)
-		.pipe(concat('script.js'))
-		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'))
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('builds/development/css'))
 });
