8501b44c825144081fe1f56b2c91e6d217c0761f
Sarah Dulat <sarah.dulat@gmail.com>
2016-12-13 15:57:27 -0500

    crushed json files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyHTML = require('gulp-minify-html'),
+    JSONminify = require('gulp-jsonminify'),
     concat = require('gulp-concat');
 
 var env,
@@ -79,7 +80,7 @@ gulp.task('watch', function() {
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
   gulp.watch('builds/development/*.html', ['html']);
-  gulp.watch(jsonSources, ['json']);
+  gulp.watch('builds/development/js/*.json', ['json']);
 });
 
 gulp.task('html', function() {
@@ -90,7 +91,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('json', function() {
-  gulp.src(jsonSources)
+  gulp.src('builds/development/js/*.json')
+  .pipe(gulpif(env === 'production', JSONminify()))
+  .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
   .pipe(connect.reload())
 });
 
